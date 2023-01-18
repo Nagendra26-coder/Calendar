5885f058660eafb4aee8f7224288f00b29f22a80
Nagendra26-coder <122618212+Nagendra26-coder@users.noreply.github.com>
2023-01-17 21:19:04 -0800

    Add files via upload

---
+++ month calendar.py
@@ -0,0 +1,14 @@
+# Python code to demonstrate the working of
+# calendar() and firstweeksday()
+
+# importing calendar module for calendar operations
+import calendar
+
+# using calendar to print calendar of year
+# prints calendar of 2012
+print ("The calendar of year 2023 is : ")
+print (calendar.calendar(2023))
+
+#using firstweekday() to print starting day number
+print ("The starting day number in calendar is : ",end="")
+print (calendar.firstweekday())
